2cf9cd039209b8ac31865e41dd1b3d4d2df69fd5
Rana Mahad Ahmer <ranamahadahmer@outlook.com>
2023-09-04 16:20:57 +0500

    Changed Layout / Organized Into Files

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'middle_widgets.dart';
+import 'rightSideBar.dart';
 import 'side_bar.dart';
-import 'top_row.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -11,143 +12,26 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  Widget secondRow() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        const Text(
-          "My Files",
-          style: TextStyle(
-              fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              backgroundColor: Colors.deepPurple),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(Icons.add),
-              Text(
-                "Add New",
-                style: TextStyle(color: Colors.white),
-              )
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget cardRow() {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Card(
-          color: Colors.white10,
-          child: SizedBox(
-            height: 180,
-            width: 200,
-          ),
-        ),
-        Card(
-          color: Colors.white10,
-          child: SizedBox(
-            height: 180,
-            width: 200,
-          ),
-        ),
-        Card(
-          color: Colors.white10,
-          child: SizedBox(
-            height: 180,
-            width: 200,
-          ),
-        ),
-        Card(
-          color: Colors.white10,
-          child: SizedBox(
-            height: 180,
-            width: 200,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget table() {
-    return const Card(
-      color: Colors.white12,
-      child: Text("      "),
-    );
-  }
-
-  Widget rightSideBar() {
-    return const Card(
-      color: Colors.white12,
-      child: Text(""),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return const Row(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        const Expanded(
+        Expanded(
           flex: 1,
           child: ColoredBox(color: Colors.black12, child: SideBar()),
         ),
         Expanded(
-          flex: 6,
+          flex: 5,
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                TopRow(),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Expanded(
-                          flex: 3,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(top: 3.0),
-                                child: secondRow(),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 10.0),
-                                child: cardRow(),
-                              ),
-                              Expanded(
-                                  child: Padding(
-                                padding: const EdgeInsets.only(top: 10.0),
-                                child: table(),
-                              )),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                            child: Padding(
-                          padding: const EdgeInsets.only(left: 10.0),
-                          child: rightSideBar(),
-                        )),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            padding: EdgeInsets.only(left: 20.0, top: 10, bottom: 10),
+            child: MiddleWidgets(),
           ),
-        )
+        ),
+        Expanded(
+          flex: 2,
+          child: RightSideBar(),
+        ),
       ],
     );
   }
---
+++ lib/middle_widgets.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class MiddleWidgets extends StatefulWidget {
+  const MiddleWidgets({super.key});
+
+  @override
+  State<MiddleWidgets> createState() => _MiddleWidgetsState();
+}
+
+class _MiddleWidgetsState extends State<MiddleWidgets> {
+  Widget _firstRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          "My Files",
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        IconButton(
+          style: IconButton.styleFrom(
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              hoverColor: Colors.blue,
+              backgroundColor: Colors.deepPurple),
+          icon: const Icon(Icons.add),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  Widget _cardRow() {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Card(
+          color: Colors.white10,
+          child: SizedBox(
+            height: 180,
+            width: 200,
+          ),
+        ),
+        Card(
+          color: Colors.white10,
+          child: SizedBox(
+            height: 180,
+            width: 200,
+          ),
+        ),
+        Card(
+          color: Colors.white10,
+          child: SizedBox(
+            height: 180,
+            width: 200,
+          ),
+        ),
+        Card(
+          color: Colors.white10,
+          child: SizedBox(
+            height: 180,
+            width: 200,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _table() {
+    return const Card(
+      color: Colors.white12,
+      child: Text("      "),
+    );
+  }
+
+  Widget _addPadding(
+      {required final EdgeInsetsGeometry pad, final Widget? child}) {
+    return Padding(padding: pad, child: child);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const Text(
+          "Dashboard",
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        _addPadding(pad: const EdgeInsets.only(top: 25.0), child: _firstRow()),
+        _addPadding(
+            pad: const EdgeInsets.symmetric(vertical: 10.0), child: _cardRow()),
+        Expanded(
+          child: _table(),
+        ),
+      ],
+    );
+  }
+}
